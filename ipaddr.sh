b566a051ec38dc8e8292602c5ee8daa06dd0f8b5
stuffbymax <96355058+stuffbymax@users.noreply.github.com>
2025-06-14 19:31:39 +0100

    Update ipaddr.sh

--- ipaddr.sh
+++ ipaddr.sh
@@ -1,2 +1,2 @@
 IPADDR=$(ip -4 a | grep inet | awk '{print $2}' | cut -d "/" -f 1 | grep -E "10\\.1\\.|192\\.168\\.")
-echo -e "your IP is \e[44m $IPADDR \e[49m"
+echo -e "your IP is \e[44m$IPADDR\e[49m"
